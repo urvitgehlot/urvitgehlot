86c592fd05c21aec5762b4eb0bb6b728134c3fa4
Urvit Gehlot <urvitgehlotug@gmail.com>
2023-12-23 20:41:43 +0530

    pc

--- scripts/main.js
+++ scripts/main.js
@@ -5,23 +5,34 @@
 const backroundBox1 = document.querySelector('.ug-background-box1');
 
 let  box1InitialRotation = 34.331
+let totalHeight = document.documentElement.scrollHeight - window.innerHeight;
 
 // backroundBox1Rotation = transform: rotate(34.331deg);
 // backroundBox1.style.transform = 'rotate(34.331deg)';
 backroundBox1.style.transform = 'rotate('+box1InitialRotation+'deg)';
 // let backroundBox1Rotation = backroundBox1.style.transform;
 
-document.addEventListener('scroll',function (event) {
-    let scrollheight = document.documentElement.scrollHeight; 
-    let scrolltop = document.documentElement.scrollTop;
-    let scrollPercent = scrolltop/ scrollheight;
-    box1InitialRotation  = scrollPercent;
-    backroundBox1.style.transform = 'rotate('+box1InitialRotation+'deg)';
-    //    let temp = box1.scrollTop;
-//    backroundBox1.style.transform = 'translateY(10%)';
-    // backroundBox1.css({transform : 'tranlateY(60%)'});
-//    box1.
-   console.log(event);
-   console.log(scrollheight);
-   console.log(scrolltop);
-});
+function boxRotation(box, toRotate) {
+    let totalRotation = (box1InitialRotation - toRotate);
+    let scrollPercent;
+    // if(box1InitialRotation < toRotate){
+        scrollPercent = window.scrollY /totalHeight * (box1InitialRotation - toRotate);
+        if(scrollPercent<=1){
+            scrollPercent = 0;
+        }
+        else{
+            scrollPercent = totalRotation - scrollPercent
+        }
+    // }
+    // else{
+        // scrollPercent = window.scrollY/ totalHeight * Math.abs(toRotate - box1InitialRotation);
+    // }
+    console.log(window.scrollY /totalHeight);
+    box.style.transform = 'rotate('+scrollPercent+'deg)';
+    console.log(toRotate);
+    
+}
+
+
+
+document.addEventListener('scroll',()=>{boxRotation(backroundBox1, 0)});
