8c1281bad67b28f062541de381312d810fee4777
Urvit Gehlot <urvitgehlotug@gmail.com>
2024-03-15 01:38:04 +0530

    pc

--- scripts/main.js
+++ scripts/main.js
@@ -2,37 +2,47 @@
 
 // Scrolling Effect
 
-const backroundBox1 = document.querySelector('.ug-background-box1');
 
-let  box1InitialRotation = 34.331
-let totalHeight = document.documentElement.scrollHeight - window.innerHeight;
 
-// backroundBox1Rotation = transform: rotate(34.331deg);
-// backroundBox1.style.transform = 'rotate(34.331deg)';
-backroundBox1.style.transform = 'rotate('+box1InitialRotation+'deg)';
-// let backroundBox1Rotation = backroundBox1.style.transform;
 
-function boxRotation(box, toRotate) {
-    let totalRotation = (box1InitialRotation - toRotate);
-    let scrollPercent;
-    // if(box1InitialRotation < toRotate){
-        scrollPercent = window.scrollY /totalHeight * (box1InitialRotation - toRotate);
-        if(scrollPercent<=1){
-            scrollPercent = 0;
-        }
-        else{
-            scrollPercent = totalRotation - scrollPercent
-        }
-    // }
-    // else{
-        // scrollPercent = window.scrollY/ totalHeight * Math.abs(toRotate - box1InitialRotation);
-    // }
-    console.log(window.scrollY /totalHeight);
-    box.style.transform = 'rotate('+scrollPercent+'deg)';
-    console.log(toRotate);
+document.addEventListener("DOMContentLoaded", function () {
+
+    let sections = document.querySelectorAll('.section');
+    let currentSection = 0;
+
+    let  box1InitialRotation = 34;
+    let totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const backroundBox1 = document.querySelector('.ug-background-box1');
+    backroundBox1.style.transform = 'rotate(34deg)';
     
-}
+    document.addEventListener("wheel",function(event) {
+        event.preventDefault();
+        const delta = event.deltaY;
+        // let percentComplete = window.scrollY / totalHeight;
+        if(delta > 0 && currentSection < sections.length){
+            console.log('down');
+            currentSection++;
+            const pos = sections[currentSection].getBoundingClientRect();
+            console.log(pos.top);
+            window.scroll({
+                top: window.scrollY + pos.top,
+                behavior: 'smooth'
+            });
+        }
+        else if (delta < 0 && currentSection > 0){
+            console.log('Up');
+            currentSection--;
+            const pos = sections[currentSection].getBoundingClientRect();
+            console.log(pos.top);
+            window.scroll({
+                top: window.scrollY + pos.top,
+                behavior: 'smooth'
+            });
+        }
 
+        // window(0,sections[currentSection].)
+        // window.scroll(0, 100);
+        // window.location.href = "#hi";
+    });
+});
 
-
-document.addEventListener('scroll',()=>{boxRotation(backroundBox1, 0)});
